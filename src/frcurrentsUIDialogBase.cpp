f0a914278ecf15ba243b24ed8670c6ef2aa15239
Ptizef <joubert.jeanpierre@orange.fr>
2025-01-06 18:41:21 +0100

    Show a short description of the areas in the dialog

--- src/frcurrentsUIDialogBase.cpp
+++ src/frcurrentsUIDialogBase.cpp
@@ -19,7 +19,10 @@ frcurrentsUIDialogBase::frcurrentsUIDialogBase( wxWindow* parent, wxWindowID id,
 	wxStaticBoxSizer* sbSizer71;
 	sbSizer71 = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, _("Tidal Area") ), wxHORIZONTAL );
 
-	wxString m_choiceAreaChoices[] = { _("557"), _("558"), _("559"), _("560"), _("561"), _("562"), _("563"), _("564"), _("565") };
+	wxString m_choiceAreaChoices[] = { _("557 Strait of Dover"), _("558 South Brittany"),
+		_("559 Vendée-Gironde"), _("560 Tip of Britanny"), _("561 Fecamp to Cherbourg"),
+		_("562 La Hague to Hx-de-Breat"), _("563 Hx-de-Breat to Brignogan"), _("564 English Channel"),
+		_("565 Bay of Biscay") };
 	int m_choiceAreaNChoices = sizeof( m_choiceAreaChoices ) / sizeof( wxString );
 	m_choiceArea = new wxChoice( sbSizer71->GetStaticBox(), wxID_ANY, wxDefaultPosition, wxDefaultSize, m_choiceAreaNChoices, m_choiceAreaChoices, 0 );
 	m_choiceArea->SetSelection( 0 );
