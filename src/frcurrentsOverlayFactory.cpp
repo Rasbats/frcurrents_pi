a1330f087525276c21aac7ad3a34e1c6cb676b73
Rasbats <miker@members.swis.net>
2025-04-19 14:37:07 +0100

    SWISS

--- src/frcurrentsOverlayFactory.cpp
+++ src/frcurrentsOverlayFactory.cpp
@@ -142,7 +142,7 @@ void frcurrentsOverlayFactory::DrawGL(PlugIn_ViewPort &piVP) {
   int j = 0;
   wxPoint r;
 
-  wxFont font(20, wxFONTFAMILY_MODERN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD,
+  wxFont font(20, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD,
               false, "Arial");
 
   g_pDC->SetFont(font);
