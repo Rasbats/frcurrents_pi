df72ca3c7ca1075be8c7826f2d619d9508e98c97
Rasbats <miker@members.swis.net>
2025-06-03 12:39:38 +0100

    prefs direct

--- src/frcurrentsUIDialog.cpp
+++ src/frcurrentsUIDialog.cpp
@@ -430,10 +430,16 @@ void frcurrentsUIDialog::SetViewPort(PlugIn_ViewPort* vp) {
 }
 
 void frcurrentsUIDialog::OnClose(wxCloseEvent& event) {
+
+  int b = m_choiceArea->GetCurrentSelection();
+  int c = m_choice1->GetCurrentSelection();
+  wxString myP = m_choice1->GetString(c);
+
+  pPlugIn->m_AreaID = b;
+  pPlugIn->m_Port = myP;
+
   m_FolderSelected = m_dirPicker1->GetValue();
   pPlugIn->m_CopyFolderSelected = m_FolderSelected;
-  pPlugIn->m_AreaID = m_AreaIDSelected;
-  pPlugIn->m_Port = m_PortSelected;
   pPlugIn->OnfrcurrentsDialogClose();
 }
 
